4631800c360bf8e1904ea0e6c244203baa78b780
Gerold Höferlin <gerold-hoeferlin@gmx.de>
2025-06-11 10:01:44 +0200

    Würfe OK

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class MainApp extends StatelessWidget {
       // Diese Funktion wird aufgerufen, wann immer eine Route aufgerufen wird, die nicht explizit in routes: registriert ist.
       // Du kannst hier dynamisch auf den Routen-String reagieren.
       onGenerateRoute: (settings) {
-        // Wandelt den Routennamen (z. B. /wettkampf/3/Zehnkampf) in ein Uri-Objekt um, 
+        // Wandelt den Routennamen (z. B. /wettkampf/3/Zehnkampf) in ein Uri-Objekt um,
         // um bequem auf Pfadsegmente (pathSegments) zuzugreifen.
         final uri = Uri.parse(settings.name ?? '');
 
@@ -75,7 +75,8 @@ class MainApp extends StatelessWidget {
         // Das zweite Segment sollte eine Zahl (Riegen-Nummer) sein und das dritte Segment sollte ein String (Wettbewerbs-Typ) sein.
         // Beispiel: /wettkampf/3/Zehnkampf
         // Wenn die Route korrekt ist, wird eine MaterialPageRoute zurückgegeben, die zur Wettbewerb-Seite führt.
-        if (uri.pathSegments.length == 3 && uri.pathSegments[0] == 'wettkampf') {
+        if (uri.pathSegments.length == 3 &&
+            uri.pathSegments[0] == 'wettkampf') {
           // Extrahiert die Riegennummer (als int) und Wettbewerbstyp (z. B. "Zehnkampf")
           final riegenNummer = int.tryParse(uri.pathSegments[1]);
           final wettbewerbsTyp = Uri.decodeComponent(uri.pathSegments[2]);
@@ -94,9 +95,8 @@ class MainApp extends StatelessWidget {
           // Beispiel: 'Sporttag - Vorab - Anmeldung'
           if (uri.pathSegments[0] == 'vorabAnmeldung') {
             return MaterialPageRoute(
-              builder: (_) => const AnmeldenVorher(
-                title: 'Sporttag - Vorab - Anmeldung')
-                );            
+                builder: (_) => const AnmeldenVorher(
+                    title: 'Sporttag - Vorab - Anmeldung'));
           }
         }
 
@@ -104,113 +104,37 @@ class MainApp extends StatelessWidget {
         switch (settings.name) {
           case 'home':
             // return MaterialPageRoute(builder: (_) => const Wettkampfbuero());
-return MaterialPageRoute(
-               builder: (_) => const Wettbewerb(
-                riegenNummer: 4,
-                wettbewerbsTyp: "Zehnkampf",
-              ));
+            return MaterialPageRoute(
+                builder: (_) => const Wettbewerb(
+                      riegenNummer: 4,
+                      wettbewerbsTyp: "Zehnkampf",
+                    ));
           case 'anmeldeSeite':
-            return MaterialPageRoute(builder: (_) => const AnmeldenSporttag(titel: 'Sporttag - Anmeldung'));
+            return MaterialPageRoute(
+                builder: (_) =>
+                    const AnmeldenSporttag(titel: 'Sporttag - Anmeldung'));
           case 'riegenEinteilung':
-            return MaterialPageRoute(builder: (_) => const RiegenEinteilung(titel: 'Riegen einteilen'));
+            return MaterialPageRoute(
+                builder: (_) =>
+                    const RiegenEinteilung(titel: 'Riegen einteilen'));
           case 'riegenZuordnung':
-            return MaterialPageRoute(builder: (_) => const RiegenZuordnung(titel: 'Riegen den Riegenführern zuordnen'));
+            return MaterialPageRoute(
+                builder: (_) => const RiegenZuordnung(
+                    titel: 'Riegen den Riegenführern zuordnen'));
           case 'auswertung':
-            return MaterialPageRoute(builder: (_) => const UrkundenDruck(titel: 'Auswerten mit Urkunden'));
+            return MaterialPageRoute(
+                builder: (_) =>
+                    const UrkundenDruck(titel: 'Auswerten mit Urkunden'));
           case 'dankeschoen':
-            return MaterialPageRoute(builder: (_) => const DankeVorabAnmeldung(titel: 'Sporttag - Vorab - Anmeldung'));
+            return MaterialPageRoute(
+                builder: (_) => const DankeVorabAnmeldung(
+                    titel: 'Sporttag - Vorab - Anmeldung'));
           default:
             return MaterialPageRoute(
-              builder: (_) => const DankeVorabAnmeldung(titel: 'Sporttag - Vorab - Anmeldung'));
+                builder: (_) => const DankeVorabAnmeldung(
+                    titel: 'Sporttag - Vorab - Anmeldung'));
         }
       },
     );
   }
 }
-
-/***********************************************
- * ältere Version, die noch nicht mit dem neuen Parse Server funktioniert
- */
-// import 'package:flutter/material.dart';
-// import 'package:logger/logger.dart';
-// import 'package:sporttag/src/riegen_zuordnung.dart';
-// import 'package:sporttag/src/urkunden.dart';
-// import 'package:sporttag/src/anmelden_sporttag.dart';
-// import 'package:sporttag/src/riegen_einteilung.dart';
-// import 'package:sporttag/src/wettkampfbuero.dart';
-// import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
-
-// import 'src/anmelden_vorher.dart';
-// import 'src/danke_schoen.dart';
-// // import 'src/wettbewerb.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   const keyApplicationId = 'WLgenML3TwDSZ80DBWggNnJaNePhJ3RQgzdCvvv0';
-//   const keyClientKey = 'LgXHwuUZDe5Dd1kuCEsz9Ui6gm30iyhgNvOVL0IM';
-//   const keyParseServerUrl = 'https://parseapi.back4app.com';
-
-//   await Parse().initialize(keyApplicationId, keyParseServerUrl,
-//       clientKey: keyClientKey, debug: true);
-
-//   //Logging ermöglichen
-//   Logger.level = Level.debug;
-
-//   runApp(const MainApp());
-// }
-
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData.light().copyWith(
-//           primaryColor: const Color.fromARGB(255, 241, 79, 15),
-//           scaffoldBackgroundColor: const Color.fromARGB(255, 246, 65, 10),
-//           appBarTheme: const AppBarTheme(
-//             backgroundColor: Color.fromARGB(255, 246, 65, 10),
-//             foregroundColor: Colors.white,
-//             titleTextStyle: TextStyle(
-//               color: Colors.white,
-//               fontSize: 38.0,
-//               fontWeight: FontWeight.bold,
-//             ),
-//           ),
-//           elevatedButtonTheme: ElevatedButtonThemeData(
-//             style: ElevatedButton.styleFrom(
-//               backgroundColor: Colors.white,
-//               foregroundColor: Colors.green,
-//               textStyle: const TextStyle(
-//                 fontSize: 24,
-//               ),
-//             ),
-//           ),
-//           textButtonTheme: TextButtonThemeData(
-//             style: TextButton.styleFrom(
-//                 backgroundColor: Colors.white, foregroundColor: Colors.green),
-//           ),
-//           inputDecorationTheme: const InputDecorationTheme(
-//             fillColor: Colors.white,
-//           )),
-//       initialRoute: 'home',
-//       routes: {
-// // zu Testzwecken auskommentiert
-//        'home': (context) => const Wettkampfbuero(),
-
-//         // 'home': (context) => const UrkundenDruck(titel: 'Auswerten mit Urkunden'),
-//         'vorabAnmeldung': (context) =>  const AnmeldenVorher(title: 'Sporttag - Vorab - Anmeldung'),
-//         'anmeldeSeite': (context) => const AnmeldenSporttag(titel: 'Sporttag - Anmeldung'),
-//         'riegenEinteilung': (context) => const RiegenEinteilung(titel: 'Riegen einteilen'),
-//         'riegenZuordnung': (context) => const RiegenZuordnung(titel: 'Riegen den Riegenführern zuordnen'),
-//         // 'wettbewerb': (context) => Wettbewerb(
-//         //       riegenNummer: savedRiegenNummer ?? 1,
-//         //       wettbewerbsTyp: savedWettbewerbsTyp ?? 'Zehnkampf',
-//         //     ),
-//         'auswertung': (context) => const UrkundenDruck(titel: 'Auswerten mit Urkunden'),
-//         'dankeschoen': (context) =>  const Dankeschoen(titel: 'Sporttag - Vorab - Anmeldung') ,
-//       },
-//     );
-//   }
-// }
