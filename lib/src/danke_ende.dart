ff97d8dd44fb4e248b8aca58275f79e1e0d3a0bf
Gerold Höferlin <gerold-hoeferlin@gmx.de>
2026-08-19 10:09:34 +0200

    Funktionierender Stand

--- lib/src/danke_ende.dart
+++ lib/src/danke_ende.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class DankeEnde extends StatelessWidget {
   const DankeEnde({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
